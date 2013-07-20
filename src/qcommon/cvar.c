ed205dbb990ec73f429c2aaf147083ddb4f740dd
zturtleman <zturtleman@gmail.com>
2013-07-20 18:15:56 +1000

    ioq3 backport: Fix setting CVAR_VM_CREATED flag on user created cvars

--- src/qcommon/cvar.c
+++ src/qcommon/cvar.c
@@ -335,6 +335,18 @@ cvar_t *Cvar_Get( const char *var_name, const char *var_value, int flags, const 
 	{
 		var_value = Cvar_Validate(var, var_value, qfalse);
 
+		// Make sure the game code cannot mark engine-added variables as gamecode vars
+		if(var->flags & CVAR_VM_CREATED)
+		{
+			if(!(flags & CVAR_VM_CREATED))
+				var->flags &= ~CVAR_VM_CREATED;
+		}
+		else
+		{
+			if(flags & CVAR_VM_CREATED)
+				flags &= ~CVAR_VM_CREATED;
+		}
+
 		// if the C code is now specifying a variable that the user already
 		// set a value for, take the new value as the reset value
 		if(var->flags & CVAR_USER_CREATED)
@@ -361,19 +373,7 @@ cvar_t *Cvar_Get( const char *var_name, const char *var_value, int flags, const 
 				var->latchedString = CopyString(var_value);
 			}
 		}
-		
-		// Make sure the game code cannot mark engine-added variables as gamecode vars
-		if(var->flags & CVAR_VM_CREATED)
-		{
-			if(!(flags & CVAR_VM_CREATED))
-				var->flags &= ~CVAR_VM_CREATED;
-		}
-		else
-		{
-			if(flags & CVAR_VM_CREATED)
-				flags &= ~CVAR_VM_CREATED;
-		}
-		
+
 		// Make sure servers cannot mark engine-added variables as SERVER_CREATED
 		if(var->flags & CVAR_SERVER_CREATED)
 		{
