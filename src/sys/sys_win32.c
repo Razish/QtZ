bd9bfdae19c91d7021bda4503c15bb176f79ad04
timangus <tim@ngus.net>
2013-07-20 18:12:14 +1000

    ioq3 backport: Fix shfolder.dll not getting freed

--- src/sys/sys_win32.c
+++ src/sys/sys_win32.c
@@ -88,44 +88,43 @@ char *Sys_DefaultHomePath( void )
 {
 	TCHAR szPath[MAX_PATH];
 	FARPROC qSHGetFolderPath;
-	HMODULE shfolder = LoadLibrary("shfolder.dll");
-	
-	if(!*homePath && com_homepath)
-	{
-		if(shfolder == NULL)
-		{
-			Com_Printf("Unable to load SHFolder.dll\n");
-			return NULL;
-		}
+	HMODULE shfolder = LoadLibrary( "shfolder.dll" );
 
+	if ( shfolder == NULL )
+	{
+		Com_Printf( "Unable to load SHFolder.dll\n" );
+		return NULL;
+	}
+
+	if ( !*homePath && com_homepath )
+	{
 		qSHGetFolderPath = GetProcAddress(shfolder, "SHGetFolderPathA");
-		if(qSHGetFolderPath == NULL)
+		if ( qSHGetFolderPath == NULL )
 		{
 			Com_Printf("Unable to find SHGetFolderPath in SHFolder.dll\n");
 			FreeLibrary(shfolder);
 			return NULL;
 		}
 
-		if( !SUCCEEDED( qSHGetFolderPath( NULL, CSIDL_APPDATA,
-						NULL, 0, szPath ) ) )
+		if ( !SUCCEEDED( qSHGetFolderPath( NULL, CSIDL_APPDATA, NULL, 0, szPath ) ) )
 		{
 			Com_Printf("Unable to detect CSIDL_APPDATA\n");
 			FreeLibrary(shfolder);
 			return NULL;
 		}
-		
+
 		Com_sprintf(homePath, sizeof(homePath), "%s%c", szPath, PATH_SEP);
 
 		if(com_homepath->string[0])
 			Q_strcat(homePath, sizeof(homePath), com_homepath->string);
 		else
 			Q_strcat(homePath, sizeof(homePath), HOMEPATH_NAME_WIN);
-
-		FreeLibrary(shfolder);
 	}
 
+	FreeLibrary(shfolder);
 	return homePath;
 }
+
 
 /*
 ================
