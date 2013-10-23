52e4193b3521118ced3190b0d163e9e7008def7e
Razish <mrrazish@gmail.com>
2013-10-23 18:56:05 +1100

    Fix bad merge from server -> client executable resulting in Linux client endlessly looping

--- src/server/sv_main.c
+++ src/server/sv_main.c
@@ -919,10 +919,10 @@ void SV_Frame( int msec ) {
 	if (!com_sv_running->integer)
 	{
 		// Running as a server, but no map loaded
-//#ifdef DEDICATED
+#ifdef DEDICATED
 		// Block until something interesting happens
 		Sys_Sleep(-1);
-//#endif
+#endif
 
 		return;
 	}
